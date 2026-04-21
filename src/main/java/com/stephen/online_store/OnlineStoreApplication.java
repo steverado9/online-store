bb713d1229bd7f8ea74b02668eb188e2b60b1e15
stephen <isaacsteverado9@gmail.com>
2026-04-21 01:46:31 +0100

    started the online store

---
+++ src/main/java/com/stephen/online_store/OnlineStoreApplication.java
@@ -0,0 +1,13 @@
+package com.stephen.online_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineStoreApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/stephen/online_store/controller/productController.java
@@ -0,0 +1,13 @@
+package com.stephen.online_store.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class productController {
+
+    @GetMapping("/")
+    public String home() {
+        return "dashboard";
+    }
+}
